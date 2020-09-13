a0be22079685546fb4c989cc27d80758f8992497
ryanzhangau <ryanzhangau@gmail.com>
2020-09-13 19:17:08 +1000

    refactor: res send

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express()
 const PORT = 80
 
 app.get('/', (req, res, next) => {
-	res.json({ message: 'Server 1 is running' })
+	res.writeHead(200)
+	res.end('Server 1 is running')
 })
 
 app.listen(PORT, () => {
